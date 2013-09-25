e5dd86c1ba7a4e0e75fc2bf3863945b23d30a789
Vladimir Canro <vcanro@gmail.com>
2013-09-25 10:52:20 -0600

    modify gemfile

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 source "https://rubygems.org"
 ruby "1.9.3"
 
-gem 'rails', '3.2.3'
+gem 'rails', '3.2.11'
 gem 'jquery-rails'
+gem 'devise'
 
 
 group :production do
--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ if defined?(Bundler)
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
-end
+  end
 
 module Omrails
   class Application < Rails::Application
